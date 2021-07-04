534f5eac294e55b359d13ad938b303be8b9cd95b
omarpecos <omarpecos@gmail.com>
2021-07-04 11:25:28 +0200

    [FIX] profile component reworked

--- src/app/auth/admin.guard.ts
+++ src/app/auth/admin.guard.ts
@@ -1,34 +1,36 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree ,Router} from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
 import { Observable } from 'rxjs';
-import {AuthService} from '../services/auth.service'
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AdminGuard implements CanActivate {
-  constructor(
-    private _authService : AuthService,
-    private _router : Router
-  ){}
+  constructor(private _authService: AuthService, private _router: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
     let identity = this._authService.getIdentity();
 
-    if (!identity){
+    if (!identity) {
       this._router.navigateByUrl('/home');
       return false;
     }
-      
-    if (identity['role'] == 1){
+
+    if (identity['role'] === 1) {
       return true;
     }
 
     this._router.navigateByUrl('/home');
     return false;
   }
-  
 }
--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,84 +1,81 @@
 import { Component, OnInit } from '@angular/core';
-import {Location} from '@angular/common';
+import { Location } from '@angular/common';
 import { SignUpBody } from 'src/app/models/SignUpBody';
-import {AuthService} from '../../services/auth.service'
+import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
+  private token;
+  private identity;
 
-private token;
-private identity;
-
-public user : SignUpBody;
-public userId : string;
-public error : string;
-public message : string;
+  public user: SignUpBody;
+  public userId: string;
+  public error: string;
+  public message: string;
 
   constructor(
-    private location : Location,
-    private _authService : AuthService,
-    private _userService : UserService,
-    private _router : Router
-  ) { 
+    private location: Location,
+    private _authService: AuthService,
+    private _userService: UserService,
+    private _router: Router
+  ) {
     this.loadCredentials();
 
-    let data : any = this.location.getState();
-    if (data._id){
+    let data: any = this.location.getState();
+    if (data._id) {
       this.userId = data._id;
       this.user = {
-        nickname : data.nickname,
-        email : data.email,
-        password : '',
-        passwordConfirmation : ''
-      }
-
-      //i think is not needed but good to have anyways
-      if (this.userId != this.identity._id && this.identity.role == 0){
-        //an normal user get here editing other user profile!
-        this._router.navigateByUrl('/home')
-      }
-    }   
+        nickname: data.nickname,
+        email: data.email,
+        password: '',
+        passwordConfirmation: '',
+      };
+    }
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  loadCredentials(){
+  loadCredentials() {
     this.token = this._authService.getToken();
     this.identity = this._authService.getIdentity();
   }
 
-  edit(){
+  edit() {
+    this.message = '';
     this.error = '';
-    
+
+    if (!this.user.password || !this.user.passwordConfirmation) {
+      delete this.user.password;
+      delete this.user.passwordConfirmation;
+    }
+
     //call to service with id,data,token
-    this._userService.editOneUser( this.userId, this.user, this.token)
-      .subscribe(
-        res =>{
-          if (res.status == 'success'){
-            
-            this.message = 'User edited successfully!';
+    this._userService.editOneUser(this.userId, this.user, this.token).subscribe(
+      (res) => {
+        if (res.status == 'success') {
+          this.message = 'User edited successfully!';
+          // update identity
+          if (this.identity._id === this.userId) {
             this._authService.setIdentity(res.data);
-
-            setTimeout(() =>{
-              this._router.navigateByUrl('/home')
-            }, 3000)
           }
-        },
-        error =>{
-          console.log(error);
-          
-          let err = error?.error;
-          this.error = err?.error || 'Internal Server Error';
-        }
-      )
-    
-  }
 
+          setTimeout(() => {
+            this._router.navigateByUrl('/home');
+          }, 3000);
+        }
+      },
+      (error) => {
+        console.log(error);
+
+        let err = error?.error;
+        this.error = err?.error || 'Internal Server Error';
+      }
+    );
+  }
 }
